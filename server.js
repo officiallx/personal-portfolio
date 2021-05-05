3946d16df5b850616cbee2f32ad87c12f76752e7
Elex <chimprockofficial@gmail.com>
2021-05-05 20:50:17 +0545

    initial commit

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/personal-portfolio'));
-
-app.listen(process.env.PORT || 8080);
