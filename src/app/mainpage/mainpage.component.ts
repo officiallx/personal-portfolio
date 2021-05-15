9db1b1206892fb176bbed0c26536cf1717892ddd
Elex <chimprockofficial@gmail.com>
2021-05-15 17:35:02 +0545

    done and dusted

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import TypeIt from 'typeit';
 
 
 @Component({
@@ -21,29 +20,5 @@ export class MainpageComponent implements OnInit {
         });
       })
     });
-
-    new TypeIt('#companionMethods', {
-      speed: 50,
-      waitUntilVisible: true
-    })
-      .type('Nvver', {delay: 1000})
-      .move(-3)
-      .delete(1)
-      .type('e')
-      .move('END')
-      .type(' let yees')
-      .pause(300)
-      .delete(2)
-      .type('sterday use up to muc')
-      .move(-4)
-      .type('o')
-      .move('END')
-      .type('h of today.')
-      .pause(500)
-      .break({delay: 500})
-      .break({delay: 1000})
-      .type('<em>- Will Rogers</em>')
-      .go();
   }
-
 }
